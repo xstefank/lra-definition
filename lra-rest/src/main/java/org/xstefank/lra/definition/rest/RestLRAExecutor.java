577e3031f72dda51583ea809cfea0ecf193883bd
xstefank <xstefank122@gmail.com>
2018-02-21 23:15:01 +0100

    Add RestLRAExecutor

---
+++ lra-rest/src/main/java/org/xstefank/lra/definition/rest/RestLRAExecutor.java
@@ -0,0 +1,23 @@
+package org.xstefank.lra.definition.rest;
+
+import org.xstefank.lra.execution.AbstractLRAExecutor;
+
+import java.net.URL;
+
+public class RestLRAExecutor extends AbstractLRAExecutor {
+
+    @Override
+    protected URL startLRA() {
+        return null;
+    }
+
+    @Override
+    protected void compensateLRA() {
+
+    }
+
+    @Override
+    protected void completeLRA() {
+
+    }
+}
